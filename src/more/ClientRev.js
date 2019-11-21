2da0c1892ad26f15b2d77a2e1cacf42c82f6432a
lsmucassi <lindasmucassi@gmail.com>
2019-11-21 22:14:27 +0200

    comps cons comps

---
+++ src/more/ClientRev.js
@@ -0,0 +1,287 @@
+import React from 'react'
+
+const ClientRev = () => {
+    return (
+         <section   class="testemonial">
+         {/* <!-- testimonial Start --> */}
+         <div class="container">
+           <div class="gallary-header text-center">
+             <h2>
+               How Our Clients Feel
+             </h2>
+             <p>
+               We are customer focused, hear what our travellers have to say
+             </p>
+           </div>
+           {/* <!--/.gallery-header--> */}
+
+           <div class="owl-carousel owl-theme" id="testemonial-carousel">
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial1.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* <!-/.home1-testm-img--> */}
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     When I plan a trip, the first people to contact is 
+                     Yellow Hat Travels, my experts, They give the best advice and tips!
+                   </p>
+                   <h3>
+                     <a >
+                       Nomusa H.
+                     </a>
+                   </h3>
+                   {/* <h4>london, england</h4>  */}
+                 </div>
+                 {/* <!--/.home1-testm-txt--> */}
+               </div>
+               {/* <!--/.home1-testm-single--> */}
+
+             </div>
+             {/* <!--/.item--> */}
+
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial2.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* <!--/.home1-testm-img--> */}
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     Yellow Hat Travels is an amazing company that curates 
+                     and plan trips. The trip planned was a bliss. Everything 
+                     was well taken care of from the day of departure until we came back.
+                   </p>
+                   <h3>
+                     <a >
+                       Lebo M.
+                     </a>
+                   </h3>
+                   {/* <h4>london, england</h4>  */}
+                 </div>
+                 {/* <!--/.home1-testm-txt--> */}
+               </div>
+               {/* <!--/.home1-testm-single--> */}
+
+             </div>
+             {/* <!--/.item--> */}
+
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial1.jpg" 
+                         alt="img"/>
+                 </div> 
+                 {/* <!--/.home1-testm-img--> */}
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     I have always had a dream to travel abroad... even 
+                     had holiday time shares ready... but still I could not move 
+                     YHT made and continues to make my dreams come true.
+                   </p>
+                   <h3>
+                     <a >
+                       Sithabile V.
+                     </a>
+                   </h3>
+                   {/* <h4>london, england</h4> */}
+                 </div>
+                 {/* <!--/.home1-testm-txt--> */}
+               </div>
+               {/* <!--/.home1-testm-single--> */}
+             </div>
+             {/* <!--/.item--> */}
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial1.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* <!--/.home1-testm-img--> */}
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     My wife and I had never left the country and Sis T held 
+                     our hands and planned everything seamlessly. We  had a blast 
+                     in Thailand and cannot wait to take our kids with next time.
+                   </p>
+                   <h3>
+                     <a >
+                       Leroy N.
+                     </a>
+                   </h3>
+                   {/* <h4>london, england</h4> */}
+                 </div>
+                 {/* <!--/.home1-testm-txt--> */}
+               </div>
+               {/* <!--/.home1-testm-single--> */}
+             </div>
+             {/* <!--/.item--> */}
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial2.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* <!--/.home1-testm-img--> */}
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     I am single lady who is full of life - I have always wanted to travel 
+                     wanted to travel but my friends would disappoint me all the time. Joining 
+                     YHT is the best thing I did for myself...Because of them I have been 
+                     to countries I never dreamt I would go to.
+                   </p>
+                   <h3>
+                     <a >
+                       Nozipho M.
+                     </a>
+                   </h3>
+                   {/* <h4>london, england</h4> */}
+                 </div>
+                 {/* <!--/.home1-testm-txt--> */}
+               </div>
+               {/* <!--/.home1-testm-single--> */}
+             </div>
+             {/* <!--/.item--> */}
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial1.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* <!-/.home1-testm-img--> */}
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     When you have a trip for the first time out of the country
+                      and that trip is USA - that can be intimidating. I Knew 
+                     nothing and I did not know where to start. YHT assisted me 
+                     from A - Z.
+                   </p>
+                   <h3>
+                     <a >
+                       Ntokozo M.
+                     </a>
+                   </h3>
+                   {/* <h4>london, england</h4> */}
+                 </div>
+                 {/* <!--/.home1-testm-txt--> */}
+               </div>
+               {/* <!--/.home1-testm-single--> */}
+             </div>
+             {/* <!--/.item--> */}
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial1.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* <!-/.home1-testm-img-> */}
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     Want a trip that is curated for you and considers your budget, 
+                     time and interest ? look no further. These people know what 
+                     they are doing. I am a collector of passport stamps thanks to 
+                     Yellow Hat Travels.
+                   </p>
+                   <h3>
+                     <a >
+                       Machael C.
+                     </a>
+                   </h3>
+                   {/* <h4>london, england</h4> */}
+                 </div>
+                 {/* <!--/.home1-testm-txt--> */}
+               </div>
+               {/* <!--/.home1-testm-single--> */}
+             </div>
+             {/* <!--/.item--> */}
+             
+             {/* <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial2.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* </.home1-testm-img /}
+               <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     Lorem ipsum dolor sit amet, contur adip elit, sed do 
+                     mod incid ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                   </p>
+                   <h3>
+                     <a href="#">
+                       kevin watson
+                     </a>
+                   </h3>
+                   <h4>london, england</h4>
+               </div>
+                 {/* <!--/.home1-testm-txt--> /}
+               </div>
+               {/* <!--/.home1-testm-single--> /}
+             </div>
+             {/* <!--/.item--> }
+             <div class="home1-testm item">
+               <div class="home1-testm-single text-center">
+                 <div class="home1-testm-img">
+                   <img src="assets/images/client/testimonial1.jpg" 
+                         alt="img"/>
+                 </div>
+                 {/* <!--/.home1-testm-img--> }
+                 <div class="home1-testm-txt">
+                   <span class="icon section-icon">
+                     <i class="fa fa-quote-left" aria-hidden="true"></i>
+                   </span>
+                   <p>
+                     Lorem ipsum dolor sit amet, contur adip elit, sed do mod 
+                     incid ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                   </p>
+                   <h3>
+                     <a href="#">
+                       kevin watson
+                     </a>
+                   </h3>
+                   <h4>london, england</h4>
+                 </div>
+                 {/* <!--/.home1-testm-txt--> }
+               </div>
+               { <!--/.home1-testm-single--> }
+           </div> */}
+             {/* <!--/.item--> */}
+
+           </div>
+           {/* <!--/.testemonial-carousel--> */}
+         </div> 
+         {/* <!--/.container--></div> */}
+         {/* // <!--/.testimonial--> */}
+         {/* // <!-- testemonial End --> */}
+       </section>
+    )
+}
+
+export default ClientRev
