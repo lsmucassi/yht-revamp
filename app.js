7c7ce1f7f8147ceccbbb78aaa9998d16858add4b
lsmucassi <lindasmucassi@gmail.com>
2020-10-01 16:39:41 +0200

    build schema

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => {
-    res.send('Hello World')
-})
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+             
+        }
+
+        type RootMutation {
+
+        }
+
+        schema {
+            query:
+            mutation:
+        }
+    `),
+    rootValue: {}
+}))
 
 app.listen(3000)
