f2bea323bf570e1410d7a3db49ee3c0a05c46c8d
lsmucassi <lindasmucassi@gmail.com>
2020-10-31 17:28:46 +0200

    schema build

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const graphqlHttp = require('express-graphql')
+const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.use('/graphql', graphqlHttp({
+app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
         type RootQuery {
-             
+             events: [String!]!
         }
 
         type RootMutation {
-
+            createEvent(name: String): String
         }
 
         schema {
-            query:
-            mutation:
+            query: RootQuery
+            mutation: RootMutation
         }
     `),
-    rootValue: {}
+    rootValue: {
+        events: () => {
+            return ['Romantic', 'Cooking', 'Sailing', 'All-Night']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
 }))
 
 app.listen(3000)
